2d722bf2d885018fecb92f0baf42111d322ce655
Ondrej Urban <urban.ondrej@volny.cz>
2025-11-15 17:17:59 +0100

    probeditor update

---
+++ apps/shared/src/lib/components/LoadingAnimation.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    let { color }: { color: string } = $props();
+
+    let size = $state(0);
+</script>
+
+<main style="--main-color: {color}; --size: {size}px">
+    <div class="block one"></div>
+    <div class="block two"></div>
+</main>
+
+<style lang="scss">
+    main {
+        width: 100%;
+        height: 100%;
+        padding-bottom: 100%;
+        box-sizing: border-box;
+        position: relative;
+
+        .block {
+            background-color: var(--main-color);
+            border-radius: 10%;
+            position: absolute;
+            animation: animation 1s cubic-bezier(0.215, 0.610, 0.355, 1) infinite;
+            width: 40%;
+            height: 40%;
+            
+            &.one {
+                animation-delay: 0s;
+            }
+
+            &.one {
+                animation-delay: -0.5s;
+            }
+        }
+    }
+
+    @keyframes animation {
+        0% {
+            top: 0%;
+            left: 0%;
+        }
+        25% {
+            top: 0%;
+            left: 60%
+        }
+        50% {
+            top: 60%;
+            left: 60%
+        }
+        75% {
+            top: 60%;
+            left: 0%;
+        }
+        100% {
+            top: 0%;
+            left: 0%;
+        }
+    }
+</style>
---
+++ apps/shared/src/lib/index.ts
@@ -0,0 +1,2 @@
+// Reexport your entry components here
+export { default as LoadingAnimation } from "./components/LoadingAnimation.svelte";
