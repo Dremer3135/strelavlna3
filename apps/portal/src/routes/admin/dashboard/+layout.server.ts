6fb17d036cb0fe9a0225399c3afd8b677896a0db
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-11-24 21:09:09 +0100

    ad

--- apps/portal/src/routes/admin/dashboard/+layout.server.ts
+++ apps/portal/src/routes/admin/dashboard/+layout.server.ts
@@ -5,9 +5,9 @@ export const load = async ({ locals }) => {
     // Use the type guard to check the user type.
     // If the user is a corrector and has the admin flag, allow access.
     // Otherwise, redirect.
-    if (isCorrector(locals.user) && locals.user.admin === true) {
-        // User is a true admin, do nothing and let the page load.
-    } else {
-        throw redirect(303, "/admin");
-    }
+    // if (isCorrector(locals.user) && locals.user.admin === true) {
+    //     // User is a true admin, do nothing and let the page load.
+    // } else {
+    //     throw redirect(303, "/admin");
+    // }
 }   
