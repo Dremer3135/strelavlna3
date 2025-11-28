9f8c5a014e0babeed83e67d42d45f8e4f0c01bd2
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-11-28 20:26:44 +0100

    asd

--- apps/enigine/managers.go
+++ apps/enigine/managers.go
@@ -130,6 +130,7 @@ func teamManager(self chan Msg, admins chan Msg, id string, tname string) {
 	correctors := map[string]chan Msg{}
 	for {
 		msg, ok := <- self
+		fmt.Printf("team: %#v\n", msg)
 		if !ok {
 			admins <- Msg{TeamChanError, id, self, ok}
 			break
@@ -344,10 +345,7 @@ func playerManager(ws *websocket.Conn, self chan Msg, team chan Msg, id string, 
 
 		case PlayerInitLoaded:
 		  data, ok := msg.data.(InitLoad)
-			if !ok {
-				fmt.Printf("not ok\n")
-				break
-			}
+			if !ok { break }
 			err := ws.WriteJSON(map[string]any{
 				"name": "initload",
 				"data": data,
