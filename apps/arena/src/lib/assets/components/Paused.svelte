d7a8e1bcefbf1bcc017102cda704293a091f00ea
Dremer3135 <114572789+Dremer3135@users.noreply.github.com>
2025-12-01 23:01:34 +0100

    update

--- apps/arena/src/lib/assets/components/Paused.svelte
+++ apps/arena/src/lib/assets/components/Paused.svelte
@@ -8,7 +8,12 @@
 {#if isPaused}
   <div class="paused-overlay">
     <div class="paused-content">
-      <h1>Soutěž je momentálně pozastavena z technických důvodů...</h1>
+      <div class="title">
+        {#each "Soutěž je pozastavena z technických důvodů...".split(" ") as word, index}
+          <h1 class="chunk" style="animation-delay: {(index * 0.1)}s; color: color-mix(in srgb, var(--color-{["pink", "orange", "yellow", "purple"][index%4]}) 90%, black 10%);">{word}</h1>
+        {/each}
+        <!-- Soutěž je momentálně pozastavena z technických důvodů... -->
+      </div>
       <p>Prosím, vyčkejte na obnovení soutěže.</p>
       <p>Na řešení problému pracujeme.</p>
       <div class="animation">
@@ -47,12 +52,35 @@
     p {
       margin: 3px;
     }
+
+    .title {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      gap: 10px;
+      
+      h1 {
+        // transform: translateY(100px);
+        animation: animation 4s cubic-bezier(0.215, 0.610, 0.355, 1) infinite;
+        color: color-mix(in srgb, var(--color-purple) 30%, black 70%);
+      }
+
+    }
   }
 
   .animation {
     margin-top: 150px;
     width: 100px;
     height: 100px;
+  }
+
+  @keyframes animation {
+    0% { transform: translateY(0px); }
+    15% { transform: translateY(0px); }
+    30% { transform: translateY(-20px); }
+    60% { transform: translateY(-20px); }
+    75% { transform: translateY(0px); }
+    100% { transform: translateY(0px); }
   }
 
 
