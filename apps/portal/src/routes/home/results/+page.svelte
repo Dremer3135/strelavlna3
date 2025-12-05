67c2e512cb447105557d5b773571aa03687f749d
PinkAntEaterMysipasi <rostislav.kozlik@seznam.cz>
2025-12-05 12:28:40 +0100

    asd

---
+++ apps/portal/src/routes/home/results/+page.svelte
@@ -0,0 +1,4 @@
+<ul>
+  <a href="https://strela-vlna.gchd.cz/home/results/strela-2025-online">Pražská Střela 2025</a>
+  <a href="https://strela-vlna.gchd.cz/home/results/vlna-2025-online">Dopplerova Vlna 2025</a>
+</ul>
---
+++ apps/portal/src/routes/home/results/vlna-2025-online/+page.svelte
@@ -0,0 +1,624 @@
+<div id="main" class="f-18">
+  <h1>Výsledky online kola Dopplerovy Vlny 2025</h1>
+  <div class="white-section">
+      <p class="blue-left">
+          Prvních 15 týmů postupuje do prezenčního kola.<br>
+          Pozn.: Týmy se záporným počtem bodů byly ze soutěže vyloučeny z důvodu porušení pravidel.
+      </p>
+      
+  </div>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Pořadí</th>
+        <th>Název</th>
+        <th>Body</th>
+      </tr>
+    </thead>
+    <tbody>
+          <tr>
+              <td>1</td>
+              <td>Lysolajská elita národa</td>
+              <td>2820</td>
+          </tr>
+          <tr>
+              <td>2</td>
+              <td>Sparta</td>
+              <td>1120</td>
+          </tr>
+          <tr>
+              <td>3</td>
+              <td>LAMY</td>
+              <td>1075</td>
+          </tr>
+          <tr>
+              <td>4</td>
+              <td>JÍLOVÁCI 1</td>
+              <td>1000</td>
+          </tr>
+          <tr>
+              <td>5</td>
+              <td>Švestkové</td>
+              <td>760</td>
+          </tr>
+          <tr>
+              <td>6</td>
+              <td>Mka</td>
+              <td>640</td>
+          </tr>
+          <tr>
+              <td>7</td>
+              <td>Lobkovicovo-4</td>
+              <td>625</td>
+          </tr>
+          <tr>
+              <td>8</td>
+              <td>Klecanská beta</td>
+              <td>580</td>
+          </tr>
+          <tr>
+              <td>9</td>
+              <td>JÍLOVÁCI 3</td>
+              <td>550</td>
+          </tr>
+          <tr>
+              <td>10</td>
+              <td>Einsteini</td>
+              <td>535</td>
+          </tr>
+          <tr>
+              <td>10</td>
+              <td>tým 1</td>
+              <td>535</td>
+          </tr>
+          <tr>
+              <td>12</td>
+              <td>BRDIČÁCI</td>
+              <td>520</td>
+          </tr>
+          <tr>
+              <td>13</td>
+              <td>Milujeme autismus</td>
+              <td>515</td>
+          </tr>
+          <tr>
+              <td>14</td>
+              <td>KKKHH</td>
+              <td>510</td>
+          </tr>
+          <tr>
+              <td>15</td>
+              <td>ty bez limitu</td>
+              <td>505</td>
+          </tr>
+          <tr>
+              <td>16</td>
+              <td>Best of Zámecká</td>
+              <td>410</td>
+          </tr>
+          <tr>
+              <td>16</td>
+              <td>Votrubáci I.</td>
+              <td>410</td>
+          </tr>
+          <tr>
+              <td>18</td>
+              <td>Klecanská gama</td>
+              <td>400</td>
+          </tr>
+          <tr>
+              <td>18</td>
+              <td>!!!Ano šéfe!!!</td>
+              <td>400</td>
+          </tr>
+          <tr>
+              <td>20</td>
+              <td>Fatální chyba</td>
+              <td>390</td>
+          </tr>
+          <tr>
+              <td>21</td>
+              <td>Srdcaři</td>
+              <td>385</td>
+          </tr>
+          <tr>
+              <td>22</td>
+              <td>Mistři</td>
+              <td>380</td>
+          </tr>
+          <tr>
+              <td>23</td>
+              <td>Sněhuláci z Čestic</td>
+              <td>370</td>
+          </tr>
+          <tr>
+              <td>23</td>
+              <td>Štrůdlteam</td>
+              <td>370</td>
+          </tr>
+          <tr>
+              <td>23</td>
+              <td>Moraváci</td>
+              <td>370</td>
+          </tr>
+          <tr>
+              <td>26</td>
+              <td>Včeličky</td>
+              <td>355</td>
+          </tr>
+          <tr>
+              <td>26</td>
+              <td>tým 3</td>
+              <td>355</td>
+          </tr>
+          <tr>
+              <td>26</td>
+              <td>Xoxouši</td>
+              <td>355</td>
+          </tr>
+          <tr>
+              <td>29</td>
+              <td>Šampioni</td>
+              <td>350</td>
+          </tr>
+          <tr>
+              <td>29</td>
+              <td>Salátové okurky</td>
+              <td>350</td>
+          </tr>
+          <tr>
+              <td>29</td>
+              <td>RYBIČKY</td>
+              <td>350</td>
+          </tr>
+          <tr>
+              <td>32</td>
+              <td>Borci</td>
+              <td>340</td>
+          </tr>
+          <tr>
+              <td>33</td>
+              <td>JÍLOVÁCI 2</td>
+              <td>335</td>
+          </tr>
+          <tr>
+              <td>33</td>
+              <td>Šturalovci</td>
+              <td>335</td>
+          </tr>
+          <tr>
+              <td>35</td>
+              <td>Agresivní okurky</td>
+              <td>315</td>
+          </tr>
+          <tr>
+              <td>36</td>
+              <td>Helikoptéry</td>
+              <td>295</td>
+          </tr>
+          <tr>
+              <td>37</td>
+              <td>Řezánci</td>
+              <td>290</td>
+          </tr>
+          <tr>
+              <td>38</td>
+              <td>Šturyteam</td>
+              <td>275</td>
+          </tr>
+          <tr>
+              <td>39</td>
+              <td>Roháčovky</td>
+              <td>265</td>
+          </tr>
+          <tr>
+              <td>40</td>
+              <td>Werich 4</td>
+              <td>260</td>
+          </tr>
+          <tr>
+              <td>41</td>
+              <td>Kalkulátoři</td>
+              <td>245</td>
+          </tr>
+          <tr>
+              <td>41</td>
+              <td>Točenákola</td>
+              <td>245</td>
+          </tr>
+          <tr>
+              <td>43</td>
+              <td>Kinder Surprise</td>
+              <td>235</td>
+          </tr>
+          <tr>
+              <td>44</td>
+              <td>Bramboráci</td>
+              <td>230</td>
+          </tr>
+          <tr>
+              <td>45</td>
+              <td>Weberovci</td>
+              <td>210</td>
+          </tr>
+          <tr>
+              <td>46</td>
+              <td>Quantum crew</td>
+              <td>195</td>
+          </tr>
+          <tr>
+              <td>46</td>
+              <td>Áčko</td>
+              <td>195</td>
+          </tr>
+          <tr>
+              <td>46</td>
+              <td>Vesničani</td>
+              <td>195</td>
+          </tr>
+          <tr>
+              <td>49</td>
+              <td>Votrubáci II.</td>
+              <td>185</td>
+          </tr>
+          <tr>
+              <td>49</td>
+              <td>Negramoti 12</td>
+              <td>185</td>
+          </tr>
+          <tr>
+              <td>49</td>
+              <td>Bobánci</td>
+              <td>185</td>
+          </tr>
+          <tr>
+              <td>52</td>
+              <td>PIX20</td>
+              <td>175</td>
+          </tr>
+          <tr>
+              <td>52</td>
+              <td>Edison</td>
+              <td>175</td>
+          </tr>
+          <tr>
+              <td>54</td>
+              <td>Švabiňáci_8</td>
+              <td>170</td>
+          </tr>
+          <tr>
+              <td>54</td>
+              <td>girls</td>
+              <td>170</td>
+          </tr>
+          <tr>
+              <td>56</td>
+              <td>princatka</td>
+              <td>165</td>
+          </tr>
+          <tr>
+              <td>57</td>
+              <td>Top Gooners</td>
+              <td>155</td>
+          </tr>
+          <tr>
+              <td>57</td>
+              <td>8B1</td>
+              <td>155</td>
+          </tr>
+          <tr>
+              <td>59</td>
+              <td>Švabiňáci_9</td>
+              <td>145</td>
+          </tr>
+          <tr>
+              <td>60</td>
+              <td>Děvčata z Bene</td>
+              <td>140</td>
+          </tr>
+          <tr>
+              <td>60</td>
+              <td>Hvězda8B</td>
+              <td>140</td>
+          </tr>
+          <tr>
+              <td>60</td>
+              <td>Exkluzivní loupáci</td>
+              <td>140</td>
+          </tr>
+          <tr>
+              <td>63</td>
+              <td>Zu Hause spielen</td>
+              <td>135</td>
+          </tr>
+          <tr>
+              <td>63</td>
+              <td>Dubeč</td>
+              <td>135</td>
+          </tr>
+          <tr>
+              <td>63</td>
+              <td>Bowlingové koule</td>
+              <td>135</td>
+          </tr>
+          <tr>
+              <td>66</td>
+              <td>Buráci</td>
+              <td>125</td>
+          </tr>
+          <tr>
+              <td>67</td>
+              <td>Skasitšušeň</td>
+              <td>120</td>
+          </tr>
+          <tr>
+              <td>67</td>
+              <td>Faraday</td>
+              <td>120</td>
+          </tr>
+          <tr>
+              <td>67</td>
+              <td>Superalfasigma</td>
+              <td>120</td>
+          </tr>
+          <tr>
+              <td>70</td>
+              <td>Šunkofleky</td>
+              <td>115</td>
+          </tr>
+          <tr>
+              <td>70</td>
+              <td>RedBull na Topu</td>
+              <td>115</td>
+          </tr>
+          <tr>
+              <td>72</td>
+              <td>Tygři</td>
+              <td>110</td>
+          </tr>
+          <tr>
+              <td>73</td>
+              <td>Tyršova 9.A</td>
+              <td>105</td>
+          </tr>
+          <tr>
+              <td>73</td>
+              <td>Bobiny</td>
+              <td>105</td>
+          </tr>
+          <tr>
+              <td>73</td>
+              <td>Newtonovy Děti</td>
+              <td>105</td>
+          </tr>
+          <tr>
+              <td>73</td>
+              <td>Chlupatý knedlíci</td>
+              <td>105</td>
+          </tr>
+          <tr>
+              <td>73</td>
+              <td>Klecanská alfa</td>
+              <td>105</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Nnd</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>dsa</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Třebotovští fyzici</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Ja</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Top</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Bossáci</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Chcípáci</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>zambo123+</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Amatéři</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>physics tym</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Borci</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Vesničani</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Mikrovlnný banditi</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>78</td>
+              <td>Černí</td>
+              <td>100</td>
+          </tr>
+          <tr>
+              <td>92</td>
+              <td>Rakety</td>
+              <td>85</td>
+          </tr>
+          <tr>
+              <td>92</td>
+              <td>9A1</td>
+              <td>85</td>
+          </tr>
+          <tr>
+              <td>94</td>
+              <td>Tesla</td>
+              <td>80</td>
+          </tr>
+          <tr>
+              <td>94</td>
+              <td>Mikrovlnkový banditi</td>
+              <td>80</td>
+          </tr>
+          <tr>
+              <td>96</td>
+              <td>Kebabáci</td>
+              <td>75</td>
+          </tr>
+          <tr>
+              <td>97</td>
+              <td>Werich 3</td>
+              <td>65</td>
+          </tr>
+          <tr>
+              <td>97</td>
+              <td>tým 2</td>
+              <td>65</td>
+          </tr>
+          <tr>
+              <td>99</td>
+              <td>Ptakopyskové periové</td>
+              <td>60</td>
+          </tr>
+          <tr>
+              <td>99</td>
+              <td>tým 6</td>
+              <td>60</td>
+          </tr>
+          <tr>
+              <td>101</td>
+              <td>Kalkulačky</td>
+              <td>55</td>
+          </tr>
+          <tr>
+              <td>102</td>
+              <td>Bomba8A</td>
+              <td>50</td>
+          </tr>
+          <tr>
+              <td>103</td>
+              <td>Čtyři mušketýři</td>
+              <td>45</td>
+          </tr>
+          <tr>
+              <td>104</td>
+              <td>Agresivní vokurky 2</td>
+              <td>40</td>
+          </tr>
+          <tr>
+              <td>105</td>
+              <td>Štěkotmňauhaf</td>
+              <td>25</td>
+          </tr>
+          <tr>
+              <td>105</td>
+              <td>Domovní bratři</td>
+              <td>25</td>
+          </tr>
+          <tr>
+              <td>105</td>
+              <td>Salámy</td>
+              <td>25</td>
+          </tr>
+          <tr>
+              <td>105</td>
+              <td>ZlatickaAC</td>
+              <td>25</td>
+          </tr>
+          <tr>
+              <td>109</td>
+              <td>Kučera a spol</td>
+              <td>10</td>
+          </tr>
+          <tr>
+              <td>110</td>
+              <td>Lysolajská elita národa</td>
+              <td>0</td>
+          </tr>
+          <tr>
+              <td>111</td>
+              <td>tým 5</td>
+              <td>-67</td>
+          </tr>
+          <tr>
+              <td>111</td>
+              <td>Lobkovicovo-1</td>
+              <td>-67</td>
+          </tr>
+          <tr>
+              <td>111</td>
+              <td>Lobkovicovo-3</td>
+              <td>-67</td>
+          </tr>
+          <tr>
+              <td>111</td>
+              <td>Lobkovicovo-2</td>
+              <td>-67</td>
+          </tr>
+          <tr>
+              <td>111</td>
+              <td>Tým 4</td>
+              <td>-67</td>
+          </tr>
+          <tr>
+              <td>111</td>
+              <td>67</td>
+              <td>-67</td>
+          </tr>
+          <tr>
+              <td>117</td>
+              <td>Zvlněný goonmaster</td>
+              <td>-67767676776767</td>
+          </tr>
+    </tbody>
+  </table>
+</div>
+
+<style>
+	table {
+		width: 100%;
+		border-collapse: collapse;
+		margin-top: 1rem;
+	}
+	th,
+	td {
+		padding: 0.5rem;
+		border: 1px solid #ccc;
+	}
+	th {
+		background-color: #f0f0f0;
+		font-weight: bold;
+	}
+</style>
